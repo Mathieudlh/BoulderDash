b6f1aef037fe92a031d30b5486afe5793978ca19
Dorian Lecoeur <contact@lecoeurd.fr>
2017-06-19 10:09:27 +0200

    d'Adrien

--- model/src/main/java/model/Player.java
+++ model/src/main/java/model/Player.java
@@ -3,5 +3,14 @@ package model;
 /**
  * Created by Utilisateur on 16/06/2017.
  */
-public class Player {
+public class Player extends Entity {
+    @Override
+    public Direction getDirection() {
+        return null;
+    }
+
+    @Override
+    public void setDirection() {
+
+    }
 }
