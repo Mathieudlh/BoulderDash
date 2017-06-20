aa541699e83dbd790f993176ff40d3a529aaa50e
Elnicoc0 <nicola.bazillio@viacesi.fr>
2017-06-20 09:27:41 +0200

    View: EventPerformer Key Order

--- view/src/main/java/view/EventPerformer.java
+++ view/src/main/java/view/EventPerformer.java
@@ -16,6 +16,26 @@ public class EventPerformer {
     }
 
     private UserOrder keyCodeToUserOrder(int keyCode) {
+
+    UserOrder userOrder;
+
+        switch (keyCode) {
+
+            case KeyEvent.VK_UP:
+                userOrder = new UserOrder(0, Order.UP);
+                break;
+            case KeyEvent.VK_RIGHT:
+                userOrder = new UserOrder(0, Order.RIGHT);
+                break;
+            case KeyEvent.VK_DOWN:
+                userOrder = new UserOrder(0, Order.DOWN);
+                break;
+            case KeyEvent.VK_LEFT:
+                userOrder = new UserOrder(0, Order.LEFT);
+                break;
+
+        }
+
         return userOrder;
     }
 
