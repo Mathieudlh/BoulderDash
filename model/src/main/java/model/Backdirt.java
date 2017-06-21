9f7e46118d43cb412216f8dbd5875504249ab2b2
Mathieudlh <mathieu.delahaye@viacesi.fr>
2017-06-21 16:12:38 +0200

    model : Backdirt and Exit added

---
+++ model/src/main/java/model/Backdirt.java
@@ -0,0 +1,8 @@
+package model;
+
+/**
+ * Created by mathi on 21/06/2017.
+ */
+public class Backdirt extends Tile {
+    public static final int TILE_NUMBER = 2;
+}
---
+++ model/src/main/java/model/Exit.java
@@ -0,0 +1,8 @@
+package model;
+
+/**
+ * Created by mathi on 21/06/2017.
+ */
+public class Exit extends Tile {
+    public static final int TILE_NUMBER = 6;
+}
