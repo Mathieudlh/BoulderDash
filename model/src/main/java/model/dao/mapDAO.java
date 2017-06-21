08baa2508eebb4f3348b7524ee991d4840ed405a
AdrienDsng <adrien.dossang@viacesi.fr>
2017-06-21 16:20:52 +0200

    Model: mapDao finished

--- model/src/main/java/model/dao/mapDAO.java
+++ model/src/main/java/model/dao/mapDAO.java
@@ -1,17 +1,31 @@
 package model.dao;
 
-import java.sql.CallableStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import static model.dao.AbstractDAO.prepareCall;
+import java.sql.*;
 
 /**
  * Created by Utilisateur on 21/06/2017.
  */
-public class mapDAO {
+public class mapDAO extends AbstractDAO{
 
-    public static String readInDB(int id) throws SQLException {
+    public mapDAO() {}
+
+    BoulderDashBDDConnector BoulderDashBDD = BoulderDashBDDConnector.getInstance();
+    Connection conn;
+    ResultSet rs = null;
+    Statement st = null;
+
+    String SqlMap, SqlHeight, SqlWidth;
+
+    public void setMap(int id){
+
+        SqlMap = "CALL callMap("+id+")";
+        SqlWidth = "CALL getWidth("+id+")";
+        SqlHeight = "CALL getHeight("+id+")";
+    }
+
+    public String readMap(int id) throws SQLException {
+
+        setMap(id);
         final CallableStatement callStatementMapCode = prepareCall("{call callMap(?)}");
         String mapCode = "";
         callStatementMapCode.setInt(1, id);
@@ -19,15 +33,12 @@ public class mapDAO {
             final ResultSet result = callStatementMapCode.getResultSet();
             if (result.first()) {
                 mapCode = result.getString(1);
+                System.out.println(mapCode);
             }
             result.close();
         }
         return mapCode;
-    public void setMap(int id){
-
-        SqlMap = "CALL callMap("+id+")";
-        SqlWidth = "CALL getWidth("+id+")";
-        SqlHeight = "CALL getHeight("+id+")";
     }
+
 
 }
