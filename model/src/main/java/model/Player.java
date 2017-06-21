deacfdb4f71d0639fa09698d42b879374e1246e2
Dorian Lecoeur <contact@lecoeurd.fr>
2017-06-21 11:39:51 +0200

    model: load image player

--- model/src/main/java/model/Player.java
+++ model/src/main/java/model/Player.java
@@ -1,7 +1,9 @@
 package model;
 
-import java.awt.*;
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by Utilisateur on 16/06/2017.
@@ -10,9 +12,14 @@ public class Player extends Entity implements Playable {
 
     private BufferedImage image;
 
-    public Player(){
-
+    public Player() {
+        try {
+            image = ImageIO.read(new File("res/textures/player.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
     @Override
     public boolean isPlayer() {
         return true;
