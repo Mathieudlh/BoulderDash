5457510d087ec4140e4a2a154be3c722b591f71c
AdrienDsng <adrien.dossang@viacesi.fr>
2017-06-21 09:23:56 +0200

    Model DAO: Database map reader

---
+++ model/src/main/java/model/dao/mapDAO.java
@@ -0,0 +1,28 @@
+package model.dao;
+
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static model.dao.AbstractDAO.prepareCall;
+
+/**
+ * Created by Utilisateur on 21/06/2017.
+ */
+public class mapDAO {
+
+    public static String readInDB(int id) throws SQLException {
+        final CallableStatement callStatementMapCode = prepareCall("{call callMap(?)}");
+        String mapCode = "";
+        callStatementMapCode.setInt(1, id);
+        if (callStatementMapCode.execute()) {
+            final ResultSet result = callStatementMapCode.getResultSet();
+            if (result.first()) {
+                mapCode = result.getString(1);
+            }
+            result.close();
+        }
+        return mapCode;
+    }
+
+}
