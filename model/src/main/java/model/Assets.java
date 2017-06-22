0621b200a42526316d231384872a4cfaa0c631c0
Elnicoc0 <nicola.bazillio@viacesi.fr>
2017-06-22 09:34:28 +0200

    Assets: drawPlayer unfix

--- model/src/main/java/model/Assets.java
+++ model/src/main/java/model/Assets.java
@@ -5,32 +5,35 @@ package model;
  */
 
 
-import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
 public class Assets {
-    
-    /*player_down = new BufferedImage[4];
+
+    /*public static final int width = 16, height = 16;
+
+    public static BufferedImage[] player_down, player_up, player_right, player_left;
+
+    player_down = new BufferedImage[4];
     player_up = new BufferedImage[4];
     player_left = new BufferedImage[3];
     player_left = new BufferedImage[3];
 
-    player_down[0] = sheet.crop(witdh , hight * 4, width, height);
-    player_down[1] = sheet.crop(witdh * 1, hight * 4, width, height);
-    player_down[2] = sheet.crop(witdh * 2 , hight * 4, width, height);
-    player_down[3] = sheet.crop(witdh * 3, hight * 4, width, height);
+    player_down[0] = sheet.crop(width , height * 4, width, height);
+    player_down[1] = sheet.crop(width * 1, height * 4, width, height);
+    player_down[2] = sheet.crop(width * 2 , height * 4, width, height);
+    player_down[3] = sheet.crop(width * 3, height * 4, width, height);
 
-    player_up[0] = sheet.crop(witdh , hight * 2, width, height);
-    player_up[1] = sheet.crop(witdh * 1, hight * 2, width, height);
-    player_up[2] = sheet.crop(witdh * 2, hight * 2, width, height);
-    player_up[3] = sheet.crop(witdh * 3, hight * 2, width, height);
+    player_up[0] = sheet.crop(width , height * 2, width, height);
+    player_up[1] = sheet.crop(width * 1, height * 2, width, height);
+    player_up[2] = sheet.crop(width * 2, height * 2, width, height);
+    player_up[3] = sheet.crop(width * 3, height * 2, width, height);
 
-    player_left[0] = sheet.crop(witdh , hight * 3, width, height);
-    player_left[1] = sheet.crop(witdh * 1, hight * 3, width, height);
-    player_left[2] = sheet.crop(witdh * 2, hight * 3, width, height);
+    player_left[0] = sheet.crop(width , height * 3, width, height);
+    player_left[1] = sheet.crop(width * 1, height * 3, width, height);
+    player_left[2] = sheet.crop(width * 2, height * 3, width, height);
 
-    player_right[0] = sheet.crop(witdh , hight, width, height);
-    player_right[1] = sheet.crop(witdh * 1, hight, width, height);
-    player_right[2] = sheet.crop(witdh * 2, hight, width, height);*/
+    player_right[0] = sheet.crop(width , height, width, height);
+    player_right[1] = sheet.crop(width * 1, height, width, height);
+    player_right[2] = sheet.crop(width * 2, height, width, height);*/
 
 }
