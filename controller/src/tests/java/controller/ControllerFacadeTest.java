0615cc3b4341f08b1852fc9d0b79e34390438d14
AdrienDsng <adrien.dossang@viacesi.fr>
2017-06-22 15:18:24 +0200

    Controller: setup controller tests

---
+++ controller/src/tests/java/controller/ControllerFacadeTest.java
@@ -0,0 +1,41 @@
+package controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Utilisateur on 22/06/2017.
+ */
+public class ControllerFacadeTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void setView() throws Exception {
+    }
+
+    @Test
+    public void start() throws Exception {
+    }
+
+    @Test
+    public void getView() throws Exception {
+    }
+
+    @Test
+    public void getModel() throws Exception {
+    }
+
+    @Test
+    public void orderPerform() throws Exception {
+    }
+
+}
