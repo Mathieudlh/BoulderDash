00eae36df10974d13f9d06e7ab71db9784ebd487
Mathieudlh <mathieu.delahaye@viacesi.fr>
2017-06-23 16:15:40 +0200

    EventPerformerTest : begin

--- view/src/tests/java/view/EventPerformerTest.java
+++ view/src/tests/java/view/EventPerformerTest.java
@@ -10,16 +10,18 @@ import static org.junit.Assert.*;
  * Created by Utilisateur on 22/06/2017.
  */
 public class EventPerformerTest {
+
+
     @Before
     public void setUp() throws Exception {
+
     }
 
-    @After
-    public void tearDown() throws Exception {
-    }
+
 
     @Test
-    public void eventPerform() throws Exception {
+    public void keyCodeToUserOrder() throws Exception {
+
     }
 
 }
